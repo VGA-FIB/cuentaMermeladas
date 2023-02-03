d8b2c54276f9f86e8758e478cfb627230892a0d9
Plopez_ <plopez.01.info@gmail.com>
2023-02-03 21:01:13 +0100

    Add basically everything

---
+++ index.js
@@ -0,0 +1,38 @@
+const mainCountdown = document.getElementById('mainCountdown');
+const closeCountdown = document.getElementById('closeCountdown');
+
+const mainDate = new Date(2023, 1, 5, 16, 0, 0).getTime();
+let closeDate = new Date();
+closeDate.setHours(21, 10, 0);
+closeDate = closeDate.getTime();
+
+function getTimeFormat(element, distance){
+    var hours = Math.floor(distance / (1000 * 60 * 60)).toString();
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString();
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000).toString();
+
+    if (hours.length == 1) hours = '0' + hours;
+    if (minutes.length == 1) minutes = '0' + minutes;
+    if (seconds.length == 1) seconds = '0' + seconds;
+
+    if (hours == 0 && !element.classList.contains("alerting")) {
+        element.classList.add("alerting");
+    }
+
+    return hours + ":" + minutes + ":" + seconds;
+    
+}
+
+setInterval(function(){
+
+    let now = new Date().getTime();
+
+    let mainDist = mainDate - now;
+
+    let closeDist = closeDate - now;
+
+    mainCountdown.innerText = getTimeFormat(mainCountdown, mainDist);
+    closeCountdown.innerText = getTimeFormat(closeCountdown, closeDist);
+
+
+}, 1000);
