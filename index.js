eaf05afab590ac003a2ed3975a77a647c5cd8fc0
Plopez_ <plopez.01.info@gmail.com>
2023-02-03 21:01:32 +0100

    Fix time

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const closeCountdown = document.getElementById('closeCountdown');
 
 const mainDate = new Date(2023, 1, 5, 16, 0, 0).getTime();
 let closeDate = new Date();
-closeDate.setHours(21, 10, 0);
+closeDate.setHours(22, 00, 0);
 closeDate = closeDate.getTime();
 
 function getTimeFormat(element, distance){
